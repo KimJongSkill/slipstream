bee4fd8193828ed73844733e7209494d1d1d454d
KimJongSkill <antroseconomides@hotmail.com>
2017-04-25 16:15:36 +0300

    Simplify JavaScipt code

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,48 +5,44 @@
     templated.co @templatedco
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
-(function($) {
-
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			global:		{ range: '*', containers: 1400, grid: { gutters: 50 } },
-			wide:			{ range: '-1680', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', containers: 960 },
-			narrow:		{ range: '-980', containers: '95%', grid: { gutters: 30 } },
-			narrower:	{ range: '-840', grid: { collapse: 1 } },
-			mobile:		{ range: '-640', containers: '90%', grid: { gutters: 15, collapse: 2 } }
-		}
-	}, {
+skel.init({
+	reset: 'full',
+	breakpoints: {
+		global:		{ range: '*', containers: 1400, grid: { gutters: 50 } },
+		wide:			{ range: '-1680', containers: 1200, grid: { gutters: 40 } },
+		normal:		{ range: '-1280', containers: 960 },
+		narrow:		{ range: '-980', containers: '95%', grid: { gutters: 30 } },
+		narrower:	{ range: '-840', grid: { collapse: 1 } },
+		mobile:		{ range: '-640', containers: '90%', grid: { gutters: 15, collapse: 2 } }
+	}
+}, {
+	layers: {
 		layers: {
-			layers: {
-				navPanel: {
-					animation: 'pushX',
-					breakpoints: 'narrower',
-					clickToClose: true,
-					height: '100%',
-					hidden: true,
-					html: '<div data-action="navList" data-args="nav"></div>',
-					orientation: 'vertical',
-					position: 'top-left',
-					side: 'left',
-					width: 275
-				},
-				titleBar: {
-					breakpoints: 'narrower',
-					height: 44,
-					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
-					position: 'top-left',
-					side: 'top',
-					width: '100%'
-				}
+			navPanel: {
+				animation: 'pushX',
+				breakpoints: 'narrower',
+				clickToClose: true,
+				height: '100%',
+				hidden: true,
+				html: '<div data-action="navList" data-args="nav"></div>',
+				orientation: 'vertical',
+				position: 'top-left',
+				side: 'left',
+				width: 275
+			},
+			titleBar: {
+				breakpoints: 'narrower',
+				height: 44,
+				html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
+				position: 'top-left',
+				side: 'top',
+				width: '100%'
 			}
 		}
-	});
+	}
+});
 
-	smoothScroll.init();
-
-})(jQuery);
+smoothScroll.init();
 
 $(document).ready( function() {
 	var Colours = [ "#2ecc71", "#e67e22", "#bdc3c7", "#f1c40f", "#95a5a6" ];
